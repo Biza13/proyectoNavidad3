9792f10b101bcb98c5e46c7ef7cf18655677b1a3
Biza_13 <serrano_0_1@hotmail.com>
2025-01-02 13:44:08 +0100

    segundo commit

--- ArchivosTf/ecrEcs.tf
+++ ArchivosTf/ecrEcs.tf
@@ -10,79 +10,95 @@ resource "aws_ecr_repository" "repositorio_ecr" {
 
 # Referencia al rol IAM existente en mi cuenta de aws que es (LabRole) usando su ARN
 data "aws_iam_role" "labrole" {
-  name = "LabRole" 
+  name = "LabRole"
 }
 
 #definición de la tarea de ecs
 resource "aws_ecs_task_definition" "apache_tarea" {
 
   #familia a la que pertenece la tarea
-  family                = "apache-tarea"
+  family = "apache-tarea"
 
-  execution_role_arn    = data.aws_iam_role.labrole.arn
-  task_role_arn         = data.aws_iam_role.labrole.arn 
+  execution_role_arn = data.aws_iam_role.labrole.arn
+  task_role_arn      = data.aws_iam_role.labrole.arn
 
   # Modo de red para Fargate
-  network_mode          = "awsvpc"  
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
 
   # Especificar recursos a nivel de la tarea
-  cpu                      = "512"
-  memory                   = "1024"
-  
-  container_definitions = jsonencode([
-    { # Primer contenedor (Apache, para la página web)
-      name      = "apache-container"
-      image     = "${aws_ecr_repository.repositorio_ecr.repository_url}:img-apachenodenpm"
-      essential = true
-      memory = 512
-      cpu = 256
+  cpu    = "512"
+  memory = "1024"
 
-      portMappings = [
+  container_definitions = <<TASK_DEFINITION
+  [
+    {
+      "cpu": 256,
+      "entryPoint": [
+        "bash",
+        "-c",
+        "apt-get update && apt-get install -y apache2 && service apache2 start && tail -f /dev/null && apt install -y nodejs && apt install -y npm && apt install nano -y && apt clean"
+      ],
+      "environment": [
+        {"name": "VARNAME", "value": "VARVAL"}
+      ],
+      "essential": true,
+      "image": "ubuntu:latest",
+      "memory": 512,
+      "name": "apache-web",
+      "portMappings": [
         {
-          containerPort = 80
-          hostPort      = 80
-          protocol      = "tcp"
+          "containerPort": 80,
+          "hostPort": 8080,
+          "protocol": "tcp"
         }
       ]
     },
-
-    { # Segundo contenedor (API JSON)
-      name      = "json-api-container"
-      image     = "${aws_ecr_repository.repositorio_ecr.repository_url}:img-jsonserver"
-      essential = true 
-      memory = 512
-      cpu = 256
-
-      portMappings = [
+    
+    {
+      "cpu": 256,
+      "entryPoint": [
+        "bash",
+        "-c",
+        "apt-get update && apt install -y nodejs && apt install -y npm && apt install nano -y && npm install -g json-server tail -f /dev/null && apt clean"
+      ],
+      "environment": [
+        {"name": "VARNAME", "value": "VARVAL"}
+      ],
+      "essential": true,
+      "image": "ubuntu:latest",
+      "memory": 512,
+      "name": "json-server",
+      "portMappings": [
         {
-          containerPort = 3000  # Puerto donde se expone el primer archivo JSON (usuarios.json)
-          hostPort      = 3000
-          protocol      = "tcp"
+          "containerPort": 3000,
+          "hostPort": 3000,  # Puerto del host para json-server en el contenedor
+          "protocol": "tcp"
         },
         {
-          containerPort = 3001  # Puerto donde se expone el segundo archivo JSON (ales.json)
-          hostPort      = 3001
-          protocol      = "tcp"
+          "containerPort": 3001,
+          "hostPort": 3001,  # Segundo puerto del host para json-server
+          "protocol": "tcp"
         },
         {
-          containerPort = 3002  # Puerto donde se expone el tercer archivo JSON (stouts.json)
-          hostPort      = 3002
-          protocol      = "tcp"
+          "containerPort": 3002,
+          "hostPort": 3002,  # Tercer puerto del host para json-server
+          "protocol": "tcp"
         }
       ]
     }
+  ]
+TASK_DEFINITION
 
-  ])
 }
 
 #balanceador de carga para que siempre tengan la misma ip publica
 resource "aws_lb" "balanceador" {
-  name               = "balanceador-de-carga"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups   = [aws_security_group.lb_security.id]
-  subnets            = [aws_subnet.subred-publica.id, aws_subnet.subred-publica-az2.id]
+  name                       = "balanceador-de-carga"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.lb_security.id]
+  subnets                    = [aws_subnet.subred-publica.id, aws_subnet.subred-publica-az2.id]
   enable_deletion_protection = false
 
   tags = {
@@ -97,9 +113,9 @@ resource "aws_lb_listener" "http" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "fixed-response"
+    type = "fixed-response"
     fixed_response {
-      status_code = 200
+      status_code  = 200
       content_type = "text/plain"
     }
   }
@@ -110,11 +126,11 @@ resource "aws_lb_listener" "http" {
 #El target group es donde el ALB dirigirá el tráfico, en este caso a las tareas de las ecs que estan ejecutando mis contenedores
 #este es el targer group para la web
 resource "aws_lb_target_group" "apache_target_group" {
-  name     = "apache-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.Desarrollo-web-VPC.id
-  target_type = "ip"  #hay que poner esto porque no estamos usando ec2 sino fargate serverless
+  name        = "apache-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.Desarrollo-web-VPC.id
+  target_type = "ip" #hay que poner esto porque no estamos usando ec2 sino fargate serverless
 
   health_check {
     path                = "/"
@@ -129,16 +145,16 @@ resource "aws_lb_target_group" "apache_target_group" {
 resource "aws_lb_listener_rule" "apache_rule" {
   listener_arn = aws_lb_listener.http.arn
 
-  priority     = 100
+  priority = 100
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.apache_target_group.arn 
+    target_group_arn = aws_lb_target_group.apache_target_group.arn
   }
 
   condition {
     path_pattern {
-      values = ["/"]  #ruta
+      values = ["/"] #ruta
     }
   }
 }
@@ -147,11 +163,11 @@ resource "aws_lb_listener_rule" "apache_rule" {
 
 # Target Group para el puerto 3000 (usuarios.json)
 resource "aws_lb_target_group" "json_target_group_3000" {
-  name     = "json-target-group-3000"
-  port     = 3000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.Desarrollo-web-VPC.id
-  target_type = "ip"  #hay que poner esto porque no estamos usando ec2 sino fargate serverless
+  name        = "json-target-group-3000"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.Desarrollo-web-VPC.id
+  target_type = "ip" #hay que poner esto porque no estamos usando ec2 sino fargate serverless
 
   health_check {
     path                = "/"
@@ -166,7 +182,7 @@ resource "aws_lb_target_group" "json_target_group_3000" {
 resource "aws_lb_listener_rule" "json_usuarios_rule" {
   listener_arn = aws_lb_listener.http.arn
 
-  priority     = 101 
+  priority = 101
 
   action {
     type             = "forward"
@@ -185,11 +201,11 @@ resource "aws_lb_listener_rule" "json_usuarios_rule" {
 
 # Target Group para el puerto 3001 (ales.json)
 resource "aws_lb_target_group" "json_target_group_3001" {
-  name     = "json-target-group-3001"
-  port     = 3001
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.Desarrollo-web-VPC.id
-  target_type = "ip"  #hay que poner esto porque no estamos usando ec2 sino fargate serverless
+  name        = "json-target-group-3001"
+  port        = 3001
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.Desarrollo-web-VPC.id
+  target_type = "ip" #hay que poner esto porque no estamos usando ec2 sino fargate serverless
 
   health_check {
     path                = "/"
@@ -204,7 +220,7 @@ resource "aws_lb_target_group" "json_target_group_3001" {
 resource "aws_lb_listener_rule" "json_ales_rule" {
   listener_arn = aws_lb_listener.http.arn
 
-  priority     = 102
+  priority = 102
 
   action {
     type             = "forward"
@@ -222,11 +238,11 @@ resource "aws_lb_listener_rule" "json_ales_rule" {
 
 # Target Group para el puerto 3002 (stouts.json)
 resource "aws_lb_target_group" "json_target_group_3002" {
-  name     = "json-target-group-3002"
-  port     = 3002
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.Desarrollo-web-VPC.id
-  target_type = "ip"  #hay que poner esto porque no estamos usando ec2 sino fargate serverless
+  name        = "json-target-group-3002"
+  port        = 3002
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.Desarrollo-web-VPC.id
+  target_type = "ip" #hay que poner esto porque no estamos usando ec2 sino fargate serverless
 
   health_check {
     path                = "/"
@@ -239,18 +255,18 @@ resource "aws_lb_target_group" "json_target_group_3002" {
 
 #Regla para enrutar tráfico al puerto 3002 (stouts.json)
 resource "aws_lb_listener_rule" "json_stouts_rule" {
-  listener_arn = aws_lb_listener.http.arn 
+  listener_arn = aws_lb_listener.http.arn
 
-  priority     = 103 
+  priority = 103
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.json_target_group_3002.arn 
+    target_group_arn = aws_lb_target_group.json_target_group_3002.arn
   }
 
   condition {
     path_pattern {
-      values = ["/stouts.json"]   #ruta
+      values = ["/stouts.json"] #ruta
     }
   }
 }
@@ -272,11 +288,11 @@ resource "aws_ecs_service" "apache_service" {
   task_definition = aws_ecs_task_definition.apache_tarea.arn
   desired_count   = 1
   launch_type     = "FARGATE"
-  
+
   network_configuration {
-    subnets          = [aws_subnet.subred-privada.id] #ponemos el servicio de la pagina en la subred privada
-    security_groups = [aws_security_group.security_ecs.id]  #ponemos el grupo de seguridad de las ecs que no permiten entrada desde internet
-    assign_public_ip = false  #para que no asigne una ip publica
+    subnets          = [aws_subnet.subred-privada.id]       #ponemos el servicio de la pagina en la subred privada
+    security_groups  = [aws_security_group.security_ecs.id] #ponemos el grupo de seguridad de las ecs que no permiten entrada desde internet
+    assign_public_ip = false                                #para que no asigne una ip publica
   }
 
   load_balancer {
@@ -293,11 +309,11 @@ resource "aws_ecs_service" "json_server_service_3000" {
   task_definition = aws_ecs_task_definition.apache_tarea.arn
   desired_count   = 1
   launch_type     = "FARGATE"
-  
+
   network_configuration {
-    subnets          = [aws_subnet.subred-privada.id] #ponemos el servicio de los json en la subred privada
-    security_groups  = [aws_security_group.security_ecs.id]  #ponemos el grupo de seguridad de las ecs que no permiten entrada desde internet
-    assign_public_ip = false  #para que no asigne una ip publica
+    subnets          = [aws_subnet.subred-privada.id]       #ponemos el servicio de los json en la subred privada
+    security_groups  = [aws_security_group.security_ecs.id] #ponemos el grupo de seguridad de las ecs que no permiten entrada desde internet
+    assign_public_ip = false                                #para que no asigne una ip publica
   }
 
   load_balancer {
@@ -314,11 +330,11 @@ resource "aws_ecs_service" "json_server_service_3001" {
   task_definition = aws_ecs_task_definition.apache_tarea.arn
   desired_count   = 1
   launch_type     = "FARGATE"
-  
+
   network_configuration {
-    subnets          = [aws_subnet.subred-privada.id] #ponemos el servicio de los json en la subred privada
-    security_groups  = [aws_security_group.security_ecs.id]  #ponemos el grupo de seguridad de las ecs que no permiten entrada desde internet
-    assign_public_ip = false  #para que no asigne una ip publica
+    subnets          = [aws_subnet.subred-privada.id]       #ponemos el servicio de los json en la subred privada
+    security_groups  = [aws_security_group.security_ecs.id] #ponemos el grupo de seguridad de las ecs que no permiten entrada desde internet
+    assign_public_ip = false                                #para que no asigne una ip publica
   }
 
   load_balancer {
@@ -335,15 +351,15 @@ resource "aws_ecs_service" "json_server_service_3002" {
   task_definition = aws_ecs_task_definition.apache_tarea.arn
   desired_count   = 1
   launch_type     = "FARGATE"
-  
+
   network_configuration {
-    subnets          = [aws_subnet.subred-privada.id] #ponemos el servicio de los json en la subred privada
-    security_groups  = [aws_security_group.security_ecs.id]  #ponemos el grupo de seguridad de las ecs que no permiten entrada desde internet
-    assign_public_ip = false  #para que no asigne una ip publica
+    subnets          = [aws_subnet.subred-privada.id]       #ponemos el servicio de los json en la subred privada
+    security_groups  = [aws_security_group.security_ecs.id] #ponemos el grupo de seguridad de las ecs que no permiten entrada desde internet
+    assign_public_ip = false                                #para que no asigne una ip publica
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.json_target_group_3002.arn 
+    target_group_arn = aws_lb_target_group.json_target_group_3002.arn
     container_name   = "json-api-container"
     container_port   = 3002
   }
