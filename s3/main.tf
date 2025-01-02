9da1cc03229a439d397e2d04c367a8c756442daf
Biza_13 <serrano_0_1@hotmail.com>
2025-01-02 17:15:40 +0100

    intento numero 2153214785478

---
+++ s3/main.tf
@@ -0,0 +1,48 @@
+#Poner el proveedor de terraform, en este caso aws
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configurar la region de aws.
+provider "aws" {
+  region = var.region
+}
+
+#--------------------S3--------------------
+resource "aws_s3_bucket" "s3"{    
+  bucket = var.s3  #nombre que le pondremos al bucket
+
+  tags = {
+    name = "bucket"
+    Enviroment = "Dev"
+  }
+}
+
+#--------------------FIN S3--------------------
+
+#--------------------VARIABLES--------------------
+variable "s3"{
+  description = "Nombre del bucket s3"
+  type = string
+  default = "cubo-s3-begona"
+}
+
+variable "region" {
+  description = "AWS Region"
+  type = string
+  default = "us-east-1"
+}
+
+#--------------------FIN VARIABLES--------------------
+
+#--------------------OUTPUTS--------------------
+output "s3" {
+  value = aws_s3_bucket.s3.id
+}
+
+#--------------------FIN OUTPUTS--------------------
